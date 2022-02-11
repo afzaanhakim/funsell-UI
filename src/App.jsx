39614c73e2cf4ce92e9ccb2b7f7cb2bb9e85eb58
afzaanhakim <afzaanhakim19@gmail.com>
2022-02-11 12:39:19 -0500

    register and login designs

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import Home from "./components/Home";
 import React, { useState } from "react";
 import ProductList from "./components/ProductList";
 import SingleProduct from "./components/SingleProduct";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+
+
 const App = () => {
-  return <div><SingleProduct/></div>;
+  return <div><Login/></div>;
 };
 
 export default App;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,18 +1,87 @@
-import React from 'react'
-
+import React from "react";
 
 import styled from "styled-components";
 
-
-
 const Container = styled.div`
-display: flex;`;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://www.pixelstalk.net/wp-content/uploads/2016/06/Free-fashion-wallpaper-hd-download.jpg");
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  width: 40%;
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  border-radius: 10px;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom:10px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap:wrap;
+  flex:1;
+  min-width: 40%;
+  margin:20px 10px 0 0;
+`;
+
+const Input = styled.input`
+  margin-bottom: 10px;
+  padding: 10px;
+
+`;
+
+const Agreement = styled.span`
+font-size:12px;
+margin:20px 0px;`;
+
+const Button = styled.button`
+width: 40%;
+border: none;
+padding: 15px 20px;
+background-color: teal;
+color: white;
+cursor: pointer;
+`;
 
 const Register = () => {
   return (
-    <Container></Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>{" "}
+        <Form>
+          <Input placeholder="first name" />
+          <Input placeholder="last name" />
+          <Input placeholder="email" />
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By Creating this account I AGREE TO THE TERMS AND CONDITIONS
+          </Agreement>
+          <Button>Create Account</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Register
+export default Register;
